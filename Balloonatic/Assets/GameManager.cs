aebd0e454c47321c79a8c5c8032d78a85319721a
elan_rubin <166179446+Elan-Rubin-2@users.noreply.github.com>
2024-09-28 16:14:17 -0400

    mat, gamemanager, textures

---
+++ Balloonatic/Assets/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //singleton pattern
+    public static GameManager Instance { get; private set; }
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+            Destroy(this);
+        else
+            Instance = this;
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
