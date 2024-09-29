0cb2eedd7ee24ca62311ff2c1ad6ba355fa99c52
elan_rubin <166179446+Elan-Rubin-2@users.noreply.github.com>
2024-09-29 01:24:47 -0400

    not finished, pushing before bed

--- Balloonatic/Assets/Scripts/PlayerMovement.cs
+++ Balloonatic/Assets/Scripts/PlayerMovement.cs
@@ -8,11 +8,15 @@ public class PlayerMovement : MonoBehaviour
     [Header("Variables")]
     [SerializeField] private float movementSpeed;
     [Header("References")]
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Transform secondHand;
     private Rigidbody2D rigidBody;
     [HideInInspector] public Vector2 PlayerPosition;
+    private bool facingDir = true;
     [HideInInspector] public bool CanMove;
     [HideInInspector] public bool Moving;
     [HideInInspector] public Vector2 currentPos, targetPos;
+    private Vector2 secondCurrentPos, secondTargetPos;
     float counter;
 
     public static PlayerMovement Instance { get; private set; }
@@ -34,6 +38,10 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.position = currentPos = Vector2.Lerp(currentPos, targetPos, Time.deltaTime * 5f);
         }
+
+        //implement raycast later
+        secondTargetPos = PlayerPosition + new Vector2(facingDir ? 1 : -1, 0) * 5f;
+        secondHand.position = secondCurrentPos = Vector3.Slerp(secondCurrentPos, secondTargetPos, Time.deltaTime * 2.5f);
     }
 
     public void SnapPosition(Vector3 newPosition)
@@ -46,12 +54,20 @@ public class PlayerMovement : MonoBehaviour
         PlayerPosition = transform.position;
         if (!CanMove) return;
         var movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        Moving = movement.magnitude > 0;
-        if (Moving && counter > 0.15f)
-        {
 
-            //play footstep sound
-            counter = 0;
+
+        Moving = movement.magnitude > 0;
+        if (Moving)
+        {
+            facingDir = movement.x < 0;
+            //facingdir true : looking left
+            spriteRenderer.transform.rotation = Quaternion.Euler(new Vector3(0, facingDir ? 0 : 180, -90));
+            //sprite flipx doesnt work
+            if (counter > 0.15f)
+            {
+                //play footstep sound here
+                counter = 0;
+            }
         }
         if (movement.magnitude > 1) movement /= movement.magnitude;
         rigidBody.velocity = movement * movementSpeed;
@@ -61,7 +77,7 @@ public class PlayerMovement : MonoBehaviour
     {
         //switch (collision.gameObject.tag)
         //{
-            
+
         //}
     }
 
