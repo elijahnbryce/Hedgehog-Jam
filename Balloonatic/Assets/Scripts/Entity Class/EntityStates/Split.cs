24e8ae5a1905a37f9bbbed228ee78d75259351a1
et402 <76020248+et402@users.noreply.github.com>
2024-10-03 19:22:48 -0400

    For Hyungwoo

---
+++ Balloonatic/Assets/Scripts/Entity Class/EntityStates/Split.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Split : EntityState 
+{
+    // Start is called before the first frame update
+    //
+    public override void FixedUpdate()
+    {
+
+    }
+}
