3685dedce68e26818ec1da8ba9dd0e8acc213fa5
elan_rubin <edr53@drexel.edu>
2024-10-06 16:41:04 -0400

    wall despawn and upgrade integration

--- Balloonatic/Assets/Scripts/EraserManager.cs
+++ Balloonatic/Assets/Scripts/EraserManager.cs
@@ -11,6 +11,7 @@ public class EraserManager : MonoBehaviour
     [SerializeField] private List<Sprite> crackedSprites = new();
     [SerializeField] private List<GameObject> configs = new();
     [SerializeField] private Material whiteMat, defaultMat;
+    [SerializeField] private ParticleSystem eraserParticles;
     public static EraserManager Instance { get; private set; }
     private void Awake()
     {
@@ -19,6 +20,7 @@ public class EraserManager : MonoBehaviour
     }
     void Start()
     {
+        //remove later
         SpawnConfig();
     }
 
@@ -34,11 +36,17 @@ public class EraserManager : MonoBehaviour
 
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            UnspawnConfig();
+        }
     }
 
     public void SpawnConfig()
     {
+        while (transform.childCount > 0)
+            Destroy(transform.GetChild(0));
+
         var chosenConfig = Instantiate(configs[Random.Range(0, configs.Count)]).transform;
         chosenConfig.parent = transform;
         chosenConfig.transform.Translate(Vector3.up * 20f);
@@ -82,7 +90,7 @@ public class EraserManager : MonoBehaviour
         }
         while (list.Count < amount)
         {
-            list.Add(Random.value > 0.5f);
+            list.Add(UnityEngine.Random.value > 0.5f);
         }
         HelperClass.Shuffle(list);
         return list;
@@ -90,6 +98,39 @@ public class EraserManager : MonoBehaviour
 
     public void UnspawnConfig()
     {
+        //Destroy(transform.GetChild(0).gameObject);
+        var ind = 0;
+        foreach (Transform child in transform)
+        {
+            ind++;
+            //child.DORotate(new Vector3(0, 0, 180), ind * 0.1f + 1f);
+            //var seq = DOTween.Sequence();
+            //seq.AppendInterval(ind * 0.1f);
+            //seq.Append(child.DOMove(child.position - Vector3.up * 20f, 1f));
 
+            foreach (Transform child2 in child)
+            {
+                var sr = child2.GetComponent<SpriteRenderer>();
+                if (sr != null)
+                    sr.material = whiteMat;
+            }
+            var seq = DOTween.Sequence();
+            seq.Append(child.DOShakePosition(0.2f, 0.6f, 30));
+            seq.Append(child.transform.DOScale(Vector3.zero, 0.15f));
+            seq.AppendCallback(() =>
+            {
+                foreach (Transform child3 in child)
+                {
+                    var newParticles = Instantiate(eraserParticles, child.position, Quaternion.identity);
+                    Destroy(newParticles, 1f);
+                }
+            });
+        }
+        var seq2 = DOTween.Sequence();
+        seq2.AppendInterval(ind * 0.1f + 1f);
+        seq2.AppendCallback(() =>
+        {
+            UpgradeManager.Instance.SpawnUpgrades();
+        });
     }
 }
