23329478f55c289b67d3a86506c7957ad2b7a628
elijahnbryce <125401109+elijahnbryce@users.noreply.github.com>
2024-10-07 00:56:19 -0400

    PowerUps and EnemyDamage

---
+++ Balloonatic/Assets/Scripts/PowerUps.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PowerUp
+{
+   None,
+   Heart,
+   Shoe,
+   Pizza,
+   Rainbow,
+   Star,
+   Lightning,
+   Fire,
+   Knife
+}
