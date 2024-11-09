5e7b6f6b0d9f1a0e0f396b223ac42bee4d15050b
ZeppelinGames <zeppelingames3@gmail.com>
2024-11-09 16:08:00 +1100

    Rubber band rework. No need for destroying and recreating band

--- TWANG/Assets/Scripts/Other Behaviors/RubberBand.cs
+++ TWANG/Assets/Scripts/Other Behaviors/RubberBand.cs
@@ -3,50 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using UnityEngine.UIElements;
+using System.IO.Pipes;
+using Unity.Burst.CompilerServices;
 
 public class RubberBand : MonoBehaviour
 {
     [SerializeField] private List<Sprite> rubberBandSprites = new();
-    [SerializeField] private GameObject landedPrefab;
+    [SerializeField] Sprite landedSprite;
+    [SerializeField] Material whiteMat;
+    [SerializeField] LayerMask boundaryLayer;
 
-    private bool dead = false;
-    [SerializeField] private Material whiteMat;
+    private bool landed = false;
     private Material defaultMat;
     private SpriteRenderer sr;
-    private float lifetime = 3f;
-    private int bounces = 0;
+    private int _maxBounces = 0;
 
-    public float initialSpeed = 5f;
-    public float spiralGrowthRate = 0.5f;
-    public float rotationSpeed = 100f;
     public float attackPower = 1f;
 
-    private int attackState;
-
     private Rigidbody2D rb;
-    void Start()
+    void Awake()
     {
-        //Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("PlayerEffect"));
-        Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("LevelBoundary"), LayerMask.NameToLayer("PlayerEffect"));
-
         sr = transform.GetChild(0).GetComponent<SpriteRenderer>();
-        defaultMat = sr.material;
-        StartCoroutine(nameof(TimedDestroy));
-
         rb = GetComponent<Rigidbody2D>();
+        defaultMat = sr.material;
     }
 
     void Update()
     {
-        if (!dead)
+        CheckBoundary();
+        if (landed) return;
+        UpdateRubberBandSprite();
+    }
+
+    void CheckBoundary()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, rb.velocity, 1f, boundaryLayer.value);
+        if (hit.collider != null)
         {
-            UpdateRubberBandSprite();
-            //Debug.Log(rb.velocity.magnitude);
-            //if (rb.velocity.magnitude < 2f)
-            //{
-            //    StartCoroutine(nameof(DestroyProjectileCoroutine));
-            //}
+            ProjectileLand();
         }
+    }
+
+    public void InitializeProjectile(Vector2 dir, int maxBounces = 1)
+    {
+        if (rb == null)
+            rb = GetComponent<Rigidbody2D>();
+
+        landed = false;
+        seq.Kill();
+        rb.isKinematic = false;
+
+        rb.AddForce(dir);
+        _maxBounces = maxBounces;
     }
 
     void UpdateRubberBandSprite()
@@ -59,7 +67,7 @@ public class RubberBand : MonoBehaviour
         if (angle > 180f)
             angle -= 180f;
 
-        int spriteIndex;
+        int spriteIndex = 4;
 
         if (angle <= 15f || angle >= 165f)  // Increased tolerance for 0/180
             spriteIndex = 4;
@@ -77,147 +85,106 @@ public class RubberBand : MonoBehaviour
             spriteIndex = 2;
         else if (angle >= 142.5f && angle < 165f)  // 150°
             spriteIndex = 3;
-        else
-            spriteIndex = 4;  // Default to horizontal sprite
+
         // Update the sprite
-        if (rubberBandSprites.Count > spriteIndex)
+        if (spriteIndex < rubberBandSprites.Count)
         {
             sr.sprite = rubberBandSprites[spriteIndex];
         }
     }
 
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (dead) return;
-
-        // Player
-        if (collision.gameObject.CompareTag("Player"))
+        switch (collision.transform.tag)
         {
-            Debug.Log("Band hit player");
-            dead = true;
-            Destroy(gameObject);
-            PlayerAttack.Instance.PickupBand();
-        }
-
-        //ass code fix later
-        if (collision.gameObject.tag == "Wall")
-        {
-            SoundManager.Instance.PlaySoundEffect("band_hit");
-            CameraManager.Instance.ScreenShake();
-            if (bounces <= 0)
-            {
-                dead = true;
-                StartCoroutine(nameof(DestroyProjectileCoroutine));
-            }
-            else
-            {
-                bounces--;
-
-                StartCoroutine(nameof(FlashWhite));
-            }
-        }
-        // idk enemy tag
-        if (collision.gameObject.tag == "Enemy")
-        {
-            collision.transform.GetComponent<Entity>().stats.TakeDamage((int)attackPower);
-
-
-            //ass code fix later
-
-            if (!GameManager.Instance.upgradeList.ContainsKey(UpgradeType.Ghost))
-            {
-                dead = true;
-                StartCoroutine(nameof(DestroyProjectileCoroutine));
-            }
-            else { GameManager.Instance.DecPowerUp(UpgradeType.Ghost); }
-        }
-    }
-
-    private bool SeekEnemies()
-    {
-        float radius = 5f;
-
-        Collider2D[] hitColliders = Physics2D.OverlapCircleAll(transform.position, radius);
-
-        bool found = false;
-        foreach (Collider2D collider in hitColliders)
-        {
-            if (!found && collider.CompareTag("Enemy"))
-            {
-                found = true;
-                var prevVelocity = rb.velocity.magnitude;
-                rb.velocity = Vector2.zero; 
-                rb.velocity = (collider.transform.position - transform.position).normalized * prevVelocity;  
-            }
-        }
-
-        return found;
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Level Boundary")
-        {
-            StartCoroutine(nameof(DestroyProjectileCoroutine));
-        }
-    }
-
-    public void InitializeProjectile(int state)
-    {
-        attackState = state;
-        switch (state)
-        {
-            case 0:
+            case "Player":
+                Debug.Log("Band hit player");
+                this.gameObject.SetActive(false);
+                PlayerAttack.Instance.PickupBand();
                 break;
-            case 1:
+            case "Wall":
+                if (landed) break;
+
+                SoundManager.Instance.PlaySoundEffect("band_hit");
+                CameraManager.Instance.ScreenShake();
+
+                // Bounce
+                if (_maxBounces <= 0)
+                    ProjectileLand();
+                if (_maxBounces > 0)
+                {
+                    transform.DOShakeScale(0.1f);
+                    _maxBounces--;
+                }
                 break;
-            case 2:
+            case "Level Boundary":
+                SoundManager.Instance.PlaySoundEffect("band_hit");
+                CameraManager.Instance.ScreenShake();
+                ProjectileLand();
                 break;
-            case 3:
+            case "Enemy":
+                if (landed) break;
+
+                if (collision.transform.TryGetComponent(out Entity e))
+                    e.stats.TakeDamage((int)attackPower);
+
+                if (!GameManager.Instance.upgradeList.ContainsKey(UpgradeType.Ghost))
+                {
+                    ProjectileLand();
+                }
+                else
+                {
+                    GameManager.Instance.DecPowerUp(UpgradeType.Ghost);
+                }
                 break;
         }
-        bounces = 1;
     }
 
-    private IEnumerator FlashWhite()
+    Sequence seq;
+    private void ProjectileLand()
     {
-        sr.material = whiteMat;
-        yield return new WaitForSeconds(0.1f);
-        sr.material = defaultMat;
-    }
+        if (landed) return;
 
-    private IEnumerator TimedDestroy()
-    {
-        yield return new WaitForSeconds(lifetime);
-        if (!dead)
-            StartCoroutine(nameof(DestroyProjectileCoroutine));
-    }
+        landed = true;
 
-    private IEnumerator DestroyProjectileCoroutine()
-    {
-        dead = true;
-
-        Destroy(GetComponent<Rigidbody2D>());
-        Destroy(GetComponent<Collider2D>());
-        //optimize later
+        rb.velocity = Vector2.zero;
+        rb.isKinematic = true;
 
         sr.material = whiteMat;
-        var seq = DOTween.Sequence();
+        sr.transform.localScale = Vector3.one;
+        seq = DOTween.Sequence();
         seq.AppendInterval(0.1f);
-        seq.Append(transform.GetChild(0).DOScale(Vector2.zero, 0.15f));
-        yield return new WaitForSeconds(0.25f);
-
-        var newBand = Instantiate(landedPrefab, transform.position, Quaternion.identity).transform;
-        //newBand.Rotate(0, 0, Random.Range(0, 3) * 90);
-        var sr2 = newBand.GetComponent<SpriteRenderer>();
-        sr2.material = whiteMat;
-        var seq2 = DOTween.Sequence();
-        seq2.Append(newBand.transform.DOPunchScale(Vector3.one * .25f, 0.15f));
-        seq2.AppendCallback(() =>
+        seq.Append(sr.transform.DOScale(Vector2.zero, 0.15f));
+        seq.AppendCallback(() =>
         {
-            sr2.material = defaultMat;
+            sr.sprite = landedSprite;
         });
-        Destroy(gameObject, 0.5f);
+        seq.Append(sr.transform.DOScale(Vector3.one, 0.15f));
+        seq.AppendCallback(() =>
+        {
+            sr.material = defaultMat;
+        });
+
+        seq.Play();
     }
+    //private bool SeekEnemies()
+    //{
+    //    float radius = 5f;
+
+    //    Collider2D[] hitColliders = Physics2D.OverlapCircleAll(transform.position, radius);
+
+    //    bool found = false;
+    //    foreach (Collider2D collider in hitColliders)
+    //    {
+    //        if (!found && collider.CompareTag("Enemy"))
+    //        {
+    //            found = true;
+    //            var prevVelocity = rb.velocity.magnitude;
+    //            rb.velocity = Vector2.zero;
+    //            rb.velocity = (collider.transform.position - transform.position).normalized * prevVelocity;
+    //        }
+    //    }
+
+    //    return found;
+    //}
 }
