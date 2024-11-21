936d4e6f8bd3a0f4573d6e79f2ac213d57c8e113
elan_rubin <elandanielrubin@gmail.com>
2024-11-21 18:26:14 -0500

    fix tack spawn visual glitch

--- TWANG/Assets/Scripts/Entity Class/EntityStates/GhostSpawn.cs
+++ TWANG/Assets/Scripts/Entity Class/EntityStates/GhostSpawn.cs
@@ -3,38 +3,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//example format of how states should be defined. Regarding which methods to implement, check EntityState.cs.
-[CreateAssetMenu(menuName = "EntityState/GhostSpawn", fileName = "GhostSpawn")] //menuName = "EntityState/StateName"
-public class GhostSpawn : EntityState //must inherit from "EntityState"
+[CreateAssetMenu(menuName = "EntityState/GhostSpawn", fileName = "GhostSpawn")]
+public class GhostSpawn : EntityState
 {
-	public AnimationCurve curve;
-	public float offset = 5;
-	
-	public float fallDuration;
+    public AnimationCurve curve;
+    public float offset = 5;
+    public float fallDuration;
 
-	public override void Initialize(Entity thisEntity, List<EntityStateChanger> stateChangers)
-	{
-		base.Initialize(thisEntity, stateChangers);
+    public override void Initialize(Entity thisEntity, List<EntityStateChanger> stateChangers)
+    {
+        base.Initialize(thisEntity, stateChangers);
 
-		selfEntity.StartCoroutine(Ghost());
-	}
+        // Disable collider immediately
+        selfEntity.physical.rootCollider.enabled = false;
 
-	public IEnumerator Ghost()
-	{
-		selfEntity.physical.rootCollider.enabled = false;
+        // Set initial position before starting the coroutine
+        Vector3 curPos = selfEntity.visual.visualObject.transform.localPosition;
+        selfEntity.visual.visualObject.transform.localPosition += new Vector3(0, offset, 0);
 
-		Vector3 curPos = selfEntity.visual.visualObject.transform.localPosition;
-		Vector3 initPos = selfEntity.visual.visualObject.transform.localPosition + new Vector3(0, offset, 0);
-        
-		
-		for (float fallTime=0; fallTime < fallDuration; fallTime += Time.deltaTime) {
-			float lerpFactor = curve.Evaluate(fallTime/fallDuration);
+        // Start the falling animation
+        selfEntity.StartCoroutine(Ghost(curPos));
+    }
 
-			selfEntity.visual.visualObject.transform.localPosition = Vector3.Lerp(initPos, curPos, lerpFactor);
-			yield return null;
-		}
-		
-		selfEntity.physical.rootCollider.enabled = true;
-	}
+    private IEnumerator Ghost(Vector3 targetPosition)
+    {
+        Vector3 startPosition = selfEntity.visual.visualObject.transform.localPosition;
+
+        for (float fallTime = 0; fallTime < fallDuration; fallTime += Time.deltaTime)
+        {
+            float lerpFactor = curve.Evaluate(fallTime / fallDuration);
+            selfEntity.visual.visualObject.transform.localPosition = Vector3.Lerp(startPosition, targetPosition, lerpFactor);
+            yield return null;
+        }
+
+        // Ensure final position is exact and enable collider
+        selfEntity.visual.visualObject.transform.localPosition = targetPosition;
+        selfEntity.physical.rootCollider.enabled = true;
+    }
 }
-
